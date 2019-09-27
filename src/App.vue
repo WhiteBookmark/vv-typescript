653b6282c760f0e532b5d2cbf395c1cbe9936b21
Sule <sule@localhost.localdomain>
2019-09-27 08:09:30 +0500

    Initial

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,39 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+	<v-row>
+		<v-col cols="1"></v-col>
+		<v-col>
+			<ValidationProvider v-slot="{ errors }" rules="required|min:30">
+				<v-text-field
+					v-model="usernameModel"
+					:error-messages="errors"
+					type="text"
+					name="Username"
+					label="Username"
+					refs="myinput"
+				></v-text-field>
+				<button @click="validateField()">Submit</button>
+			</ValidationProvider>
+		</v-col>
+		<v-col cols="1"></v-col>
+	</v-row>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import { ValidationProvider } from 'vee-validate';
+
+@Component
+export default class App extends Vue {
+  public usernameModel: string = 'Default';
+
+  public $refs!: {
+	myinput: InstanceType<typeof ValidationProvider>;
+  };
+
+  private async validateField() {
+	// Validate the field
+	const result = await this.$refs.myinput.validate();
   }
 }
-</style>
+</script>
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,26 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import { ValidationProvider, extend } from 'vee-validate';
+import { required, email, min } from 'vee-validate/dist/rules';
+import vuetify from './plugins/vuetify';
+
+// Add the required rules
+extend('required', {
+	...required,
+	message: 'This field is required',
+});
+extend('email', email);
+extend('min', min);
+
+// Register it globally
+Vue.component('ValidationProvider', ValidationProvider);
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: (h) => h(App),
+	router,
+	store,
+	vuetify,
+	render: (h) => h(App),
 }).$mount('#app');
